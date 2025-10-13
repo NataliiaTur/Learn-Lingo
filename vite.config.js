920c068cc495ab673c0bd86ea047755602220b18
Nataliia Tur <natalim@ukr.net>
2025-10-13 20:50:18 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "@assets": path.resolve("src/assets"),
