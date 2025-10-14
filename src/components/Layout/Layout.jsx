35e5c22d4e9759653e2ed0a665ce9ab27315d256
Nataliia Tur <natalim@ukr.net>
2025-10-14 19:13:06 +0300

    added HomePage with styles and styles for header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,6 +6,7 @@ import Modal from "@components/Modal/Modal.jsx";
 import LoginForm from "@components/AuthForms/LoginForm.jsx";
 import RegistrationForm from "@components/AuthForms/RegistrationsForms.jsx";
 import css from "./Layout.module.css";
+import { Container } from "../Container/Container.jsx";
 
 function Layout() {
   const { currentUser } = useAuth();
@@ -42,57 +43,67 @@ function Layout() {
   return (
     <div>
       <header className={css.header}>
-        <nav className={css.nav}>
-          <div className={css.logo}>
-            <span>Learn Lingo</span>
-          </div>
-
-          <div className={css.links}>
-            <Link to="/" className={css.link}>
-              Home
+        <Container>
+          <nav className={css.nav}>
+            <Link to="/" className={css.logo}>
+              <svg className={css.logoIcon} width="28" height="28">
+                <use href="icons.svg#icon-logo"></use>
+              </svg>
+              <span>LearnLingo</span>
             </Link>
-            <Link to="/teachers" className={css.link}>
-              Teachers
-            </Link>
-            {currentUser && (
-              <Link to="/favorites" className={css.link}>
-                Favorites
-              </Link>
-            )}
-          </div>
 
-          <div className={css.authButtons}>
-            {currentUser ? (
-              <>
-                <span className={css.userName}>
-                  {currentUser.displayName || currentUser.email}
-                </span>
-                <button
-                  onClick={handleLogout}
-                  className={css.logoutButton}
-                  disabled={isLoggingOut}
-                >
-                  {isLoggingOut ? "Вихід..." : "Вийти"}
-                </button>
-              </>
-            ) : (
-              <>
-                <button
-                  onClick={() => setIsLoginModalOpen(true)}
-                  className={css.loginButton}
-                >
-                  Вхід
-                </button>
-                <button
-                  onClick={() => setIsRegisterModalOpen(true)}
-                  className={css.registerButton}
-                >
-                  Реєстрація
-                </button>
-              </>
-            )}
-          </div>
-        </nav>
+            <div className={css.linksWrapper}>
+              <div className={css.links}>
+                <Link to="/" className={css.link}>
+                  Home
+                </Link>
+                <Link to="/teachers" className={css.link}>
+                  Teachers
+                </Link>
+                {currentUser && (
+                  <Link to="/favorites" className={css.link}>
+                    Favorites
+                  </Link>
+                )}
+              </div>
+            </div>
+
+            <div className={css.authButtons}>
+              {currentUser ? (
+                <>
+                  <span className={css.userName}>
+                    {currentUser.displayName || currentUser.email}
+                  </span>
+                  <button
+                    onClick={handleLogout}
+                    className={css.logoutButton}
+                    disabled={isLoggingOut}
+                  >
+                    {isLoggingOut ? "Вихід..." : "Вийти"}
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    onClick={() => setIsLoginModalOpen(true)}
+                    className={css.loginButton}
+                  >
+                    <svg className={css.icon} width="20" height="20">
+                      <use href="/icons.svg#icon-log-in"></use>
+                    </svg>
+                    Log in
+                  </button>
+                  <button
+                    onClick={() => setIsRegisterModalOpen(true)}
+                    className={css.registerButton}
+                  >
+                    Registration
+                  </button>
+                </>
+              )}
+            </div>
+          </nav>
+        </Container>
       </header>
 
       <main>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,67 @@
 import css from "./Home.module.css";
 import React from "react";
+import { Link } from "react-router-dom";
+import blockImage from "@assets/images/block.webp";
+import { Container } from "@components/Container/Container.jsx";
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to Learn Lingo</p>
-    </div>
+    <section className={css.homeSection}>
+      <Container className={css.homeContainer}>
+        <div className={css.homeDescWithImg}>
+          <div className={css.homeBlockDesc}>
+            <h1 className={css.homeTitle}>
+              Unlock your potential with the best{" "}
+              <span className={css.homeTitleWord}>language</span> tutors
+            </h1>
+            <p className={css.homeText}>
+              Embark on an Exciting Language Journey with Expert Language
+              Tutors: Elevate your language proficiency to new heights by
+              connecting with highly qualified and experienced tutors.
+            </p>
+            <Link to="/teachers" className={css.homeButton}>
+              Get started
+            </Link>
+          </div>
+          <div className={css.homeBlockImg}>
+            <img src={blockImage} alt="Woman with laptop" />
+          </div>
+        </div>
+
+        <div className={css.homeListBorder}>
+          <ul className={css.homeListBenefits}>
+            <li className={css.homeElementOfListBenefits}>
+              <p className={css.homeBenefitsNumber}>
+                <span>32,000</span>
+                <span>+</span>
+              </p>
+              <p className={css.homeBenefitsText}>Experienced tutors</p>
+            </li>
+            <li className={css.homeElementOfListBenefits}>
+              <p className={css.homeBenefitsNumber}>
+                <span>300,000</span>
+                <span>+</span>
+              </p>
+              <p className={css.homeBenefitsText}>5-star tutor reviews</p>
+            </li>
+            <li className={css.homeElementOfListBenefits}>
+              <p className={css.homeBenefitsNumber}>
+                <span>120</span>
+                <span>+</span>
+              </p>
+              <p className={css.homeBenefitsText}>Subjects taught</p>
+            </li>
+            <li className={css.homeElementOfListBenefits}>
+              <p className={css.homeBenefitsNumber}>
+                <span>200</span>
+                <span>+</span>
+              </p>
+              <p className={css.homeBenefitsText}>Tutor nationalities</p>
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </section>
   );
 }
 
